5456474d19904338f5246a978b8771b45ecdf4ba
Shaan Fatima <shaanfatima5@gmail.com>
2024-11-07 22:58:14 +0530

    json

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; 
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
-
-server.listen(port);
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running');
+});
